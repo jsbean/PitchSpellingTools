31ff996348a26a86449ae8ddd1a2708c5ce06c72
jsbean <jbean46@gmail.com>
2016-05-11 20:22:19 -0400

    Clean up whitespace

--- PitchSpellingTools/IntervalQuality.swift
+++ PitchSpellingTools/IntervalQuality.swift
@@ -14,39 +14,6 @@ import Pitch
 
 
 
-internal protocol ImperfectIntervalQualityType: IntervalQualityType {
-    static var major: IntervalQuality.EnumKind { get }
-    static var minor: IntervalQuality.EnumKind { get }
-    static var imperfectMembers: [IntervalQuality.EnumKind] { get }
-}
-
-extension ImperfectIntervalQualityType {
-    
-    static var imperfectMembers: [IntervalQuality.EnumKind] {
-        return [diminished, minor, major, augmented]
-    }
-    
-    static var stepPreserving: [IntervalQuality.EnumKind] { return [minor, major] }
-    
-    static func adjustDifference(difference: Float,
-        forLowerPitchSpelling pitchSpelling: PitchSpelling
-    ) -> Float
-    {
-        return [.b, .e].contains(pitchSpelling.letterName) ? difference - 1 : difference
-    }
-    
-    static func intervalQuality(fromDirectionDifference difference: Float)
-        -> IntervalQuality.EnumKind
-    {
-        switch difference {
-        case -2: return diminished
-        case -1: return minor
-        case +0: return major
-        case +1: return augmented
-        default: fatalError("Such an interval couldn't possibly exist")
-        }
-    }
-}
 
 /**
  Quality of Interval between two `SpelledPitch` objects.
