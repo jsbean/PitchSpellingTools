b2989434527b8dcd6668b87270b8c1080a50947a
jsbean <jbean46@gmail.com>
2016-05-24 11:37:09 -0400

    Add Edge init phase

--- Edge.swift
+++ Edge.swift
@@ -10,5 +10,11 @@ import Foundation
 
 internal final class Edge {
     
+    private let before: Node
+    private let after: Node
     
+    internal init(nodeBefore before: Node, nodeAfter after: Node) {
+        self.before = before
+        self.after = after
+    }
 }
