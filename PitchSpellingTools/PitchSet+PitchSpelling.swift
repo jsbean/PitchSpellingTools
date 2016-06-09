b2ff89a40894d548e6b8f7d39ca57918c48ed066
jsbean <jbean46@gmail.com>
2016-06-09 15:00:47 +0200

    Add doc comments

--- PitchSpellingTools/PitchSet+PitchSpelling.swift
+++ PitchSpellingTools/PitchSet+PitchSpelling.swift
@@ -10,9 +10,10 @@ import Pitch
 
 extension PitchSet {
     
+    // MARK: PitchSpelling
     
+    /// The degree to which a `PitchSet` can be spelled.
     public var spellability: Spellability {
-        
         if isEmpty || self.allSatisfy({ $0.canBeSpelledObjectively }) {
             return .objective
         } else if anySatisfy({ $0.canBeSpelledObjectively }) {
@@ -22,6 +23,13 @@ extension PitchSet {
         }
     }
     
+    /**
+     - throws: `PitchSpelling.Error` if any `Pitch` values herein cannot be spelled with
+     current technology.
+     
+     - returns: `SpelledPitchSet` containing `SpelledPitch` values for each `Pitch` value
+     contained herein.
+     */
     public func spelledWithDefaultSpellings() throws -> SpelledPitchSet {
         return try SpelledPitchSet(self.map { try $0.spelledWithDefaultSpelling() })
     }
