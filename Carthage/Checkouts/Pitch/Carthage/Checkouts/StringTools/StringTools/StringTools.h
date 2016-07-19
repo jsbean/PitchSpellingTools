c76b9620d446bfa6ae1aea12264b64f614659d74
jsbean <jbean46@gmail.com>
2016-07-19 20:14:35 +0200

    Remove PitchTools PBXReference

--- Carthage/Checkouts/Pitch/Carthage/Checkouts/StringTools/StringTools/StringTools.h
+++
@@ -1,7 +0,0 @@
-//
-//  StringTools.h
-//  StringTools
-//
-//  Created by James Bean on 2/24/16.
-//  Copyright © 2016 James Bean. All rights reserved.
-//
--- Carthage/Checkouts/TreeTools/TreeTools/TreeTools.h
+++
@@ -1,7 +0,0 @@
-//
-//  TreeTools.h
-//  TreeTools
-//
-//  Created by James Bean on 3/1/16.
-//  Copyright © 2016 James Bean. All rights reserved.
-//
