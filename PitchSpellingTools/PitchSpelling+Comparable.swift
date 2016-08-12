39b87050ba6382073ab26129263e3ac3cf7b5e1b
jsbean <jbean46@gmail.com>
2016-08-12 11:49:58 +0200

    Add doc comment

--- PitchSpellingTools/PitchSpelling+Comparable.swift
+++ PitchSpellingTools/PitchSpelling+Comparable.swift
@@ -10,6 +10,15 @@ import Foundation
 
 extension PitchSpelling: Comparable { }
 
+// MARK: - Comparable
+
+/**
+ - returns: `true` if the `PitchSpelling` value on the left has a lesser `letterName` value. In
+ the case that the `letterName` values are equivalent, `true` if the `PitchSpelling` value on 
+ the left has a lesser `quarterStep` value. In the case that the `quarterStep` values are
+ equivalent, `true` if the `PitchSpelling` value on the left has a lesser `eighthStep` value.
+ Otherwise, `false`.
+ */
 public func < (lhs: PitchSpelling, rhs: PitchSpelling) -> Bool {
     if lhs.letterName.steps < rhs.letterName.steps { return true }
     if lhs.letterName.steps == rhs.letterName.steps {
